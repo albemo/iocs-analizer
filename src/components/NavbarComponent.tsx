5b9f970dc7dec09d8880fe5bba4c4fe6dc429f4f
Alexander Becerra <desarrolladorjunior2@tht.company>
2020-10-09 16:52:21 -0500

    make it better

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap';
+import { RiVirusLine } from 'react-icons/ri'
 
 function NavbarComponent() {
   return (
     <>
       <Navbar bg="success" variant="dark" expand="lg">
-        <Navbar.Brand href="/home">IOCs Analizer</Navbar.Brand>
+        <RiVirusLine size={32} />
+        <Navbar.Brand className='ml-2' href="/">IOCs Analizer</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        {/* <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="/virus-total">Virus Total</Nav.Link>
             <Nav.Link href="/otro">Otro</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
+        </Navbar.Collapse> */}
       </Navbar>
     </>
   )
