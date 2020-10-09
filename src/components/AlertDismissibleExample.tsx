7385bd4632267102fb6bd98983dc12cdd78e46aa
Alexander Becerra <desarrolladorjunior2@tht.company>
2020-10-09 16:25:17 -0500

    to deploy

---
+++ src/components/AlertDismissibleExample.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Alert } from 'react-bootstrap'
+
+function AlertDismissibleExample({ errorMessage }: { errorMessage: string }) {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh noo! Tienes un error!</Alert.Heading>
+        <p> {errorMessage} </p>
+      </Alert>
+    );
+  }
+  return (
+    <>
+    </>
+  );
+}
+
+export default AlertDismissibleExample
