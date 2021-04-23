4a80d5bf368ade3fe1f38b049638378bbb5fe44d
Alexander Becerra <desarrollador@tht.company>
2021-04-23 15:36:34 -0500

    make it better

--- src/components/ioc/IocComponent.tsx
+++ src/components/ioc/IocComponent.tsx
@@ -35,7 +35,7 @@ function IocComponentP() {
   const refresh = async () => {
     let iocItems: IiocItem[] = [];
     try {
-      let data = await axios.get('https://localhost:5001/api/v1/iocs');
+      let data = await axios.get('http://localhost:5000/api/v1/iocs');
       iocItems = data.data;
       setIocs(iocItems);
     } catch (error) {
@@ -58,9 +58,9 @@ function IocComponentP() {
     setButtonDisabled(true);
     setTextareaDisabled(true);
     try {
-      const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+      //const proxyurl = 'https://cors-anywhere.herokuapp.com/';
       const fetchIoc = await axios(
-        `${proxyurl}https://www.virustotal.com/api/v3/files/${hash}`,
+        `https://www.virustotal.com/api/v3/files/${hash}`,
         {
           headers: { 'x-apiKey': `${process.env.REACT_APP_API_KEY}` },
         }
@@ -135,7 +135,7 @@ function IocComponentP() {
 
   const submit = async (item: IiocItem) => {
     let iocItems: IiocItem[] = [];
-    let iocs = await axios.get('https://localhost:5001/api/v1/iocs');
+    let iocs = await axios.get('http://localhost:5000/api/v1/iocs');
     iocItems = iocs.data;
     let isExist = iocItems.some(
       (i) =>
@@ -156,7 +156,7 @@ function IocComponentP() {
     }
 
     await axios
-      .post('https://localhost:5001/api/v1/iocs', item)
+      .post('http://localhost:5000/api/v1/iocs', item)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
 
