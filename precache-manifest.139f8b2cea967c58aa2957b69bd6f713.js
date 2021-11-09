fdbefca2d0264f169117bc08f501a78f7620380f
Alexander Becerra Moreno <Abmoreno@tuya.com.co>
2021-11-08 19:35:09 -0500

    Updates

---
+++ precache-manifest.139f8b2cea967c58aa2957b69bd6f713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e45516e5596bc66d30e56e02296722f",
+    "url": "/iocs-analizer/index.html"
+  },
+  {
+    "revision": "9ebec26fb49900d8a9ed",
+    "url": "/iocs-analizer/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "4917daa804d21821c13b",
+    "url": "/iocs-analizer/static/css/main.0bbeb6f4.chunk.css"
+  },
+  {
+    "revision": "9ebec26fb49900d8a9ed",
+    "url": "/iocs-analizer/static/js/2.ab2b3ef4.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/iocs-analizer/static/js/2.ab2b3ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4917daa804d21821c13b",
+    "url": "/iocs-analizer/static/js/main.9bb8e4bc.chunk.js"
+  },
+  {
+    "revision": "2b0ab377f9bdead6b205",
+    "url": "/iocs-analizer/static/js/runtime-main.45c622e7.js"
+  }
+]);
